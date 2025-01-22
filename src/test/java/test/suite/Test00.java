534283010555fcda88e38c882be67dd2dec2c4ac
DisIsNotMe <orumighto02@gmail.com>
2025-01-22 09:20:41 +0800

    second commit

--- src/test/java/test/suite/Test00.java
+++ src/test/java/test/suite/Test00.java
@@ -1,9 +1,11 @@
 package test.suite;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -13,17 +15,25 @@ public class Test00 {
 	public WebDriver driver;
 	public String scenario;
 	
+	public WebDriverWait wait;
+		
 	@BeforeTest
 	public void init() {
 		WebDriverManager.chromedriver().setup();
 
 		driver = new ChromeDriver();
 		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		
         driver.manage().window().maximize();
 
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	
-	
+	@AfterTest
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(5000);
+		driver.close();
+	}
 	
 }
--- src/test/java/test/suite/Test01_Google.java
+++ src/test/java/test/suite/Test01_Google.java
@@ -1,12 +1,23 @@
 package test.suite;
 
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
 import org.testng.annotations.*;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class Test01_Google extends Test00{
 	
 	@Test
 	public void test01() {
 		driver.get("https://www.google.com/");
+		
+		WebElement searchBar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"APjFqb\"]")));
+		
+		assertEquals(searchBar.isDisplayed(), true);
+		
 	}
 	
 }
